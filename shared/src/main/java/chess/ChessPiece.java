659c04404dd1b289bd5b5ff5467b5cdefa6a5942
“Tyler <tylerwilcock05@gmail.com>
2026-09-19 18:44:48 -0600

    declared variables for pieceMoves function in ChessPiece.java and first bishopMoveUntilEdge() test is passing

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
@@ -56,8 +57,34 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
 
         ChessPiece piece = board.getPiece(myPosition);
+        Collection<ChessMove> validMovesArray = new ArrayList<>(27);
+        int validMoves = 0;
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        int i;
+        int j;
+        int dirX = 1;
+        int dirY = 1;
+        int newXPos;
+        int newYPos;
         if (piece.getPieceType() == PieceType.BISHOP) {
-            return List.of(new ChessMove(new ChessPosition(5, 4), new ChessPosition(1, 8), null));
+            for (i = 0; i < 4; i++) {
+                for (j = 1; j < 8; j++) {
+                    newXPos = row + dirY * j;
+                    newYPos = col + dirX * j;
+                    if (newXPos > 8 || newXPos < 1 || newYPos > 8 || newYPos < 1) {
+                        break;
+                    }
+                    validMovesArray.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(newXPos, newYPos), null));
+                }
+                if (i % 2 == 0) {
+                    dirX = dirX * -1;
+                }
+                else {
+                    dirY = dirY * -1;
+                }
+            }
+            return validMovesArray;
         }
         return List.of();
     }
